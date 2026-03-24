629352b201e83c416298e595d2e15e8856bab83b
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-03-24 11:36:25 +0900

    feat: TodoList 실습 내용 추가

---
+++ TIL/260324_DAY15_TodoList_Project/todolist-app/src/components/InputTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row mb-3">
+    <div class="col">
+      <div class="input-group">
+        <input 
+          type="text" 
+          class="form-control"
+          placeholder="할일을 여기에 입력!"
+          v-model.trim="todo"
+          @keyup.enter="addTodoHandler" 
+        />
+        <span 
+          class="btn btn-primary input-group-addon"
+          @click="addTodoHandler"
+        >
+          추가
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputTodo",
+  data() {
+    return { 
+      todo: "" 
+    };
+  },
+  // 부모에게 보낼 이벤트 이름을 명시적으로 선언 (Vue 3 권장사항)
+  emits: ["add-todo"],
+  methods: {
+    addTodoHandler() {
+      // 3글자 이상일 때만 이벤트 발신
+      if (this.todo.length >= 3) {
+        this.$emit('add-todo', this.todo);
+        this.todo = ""; // 입력창 초기화
+      }
+    }
+  },
+};
+</script>
---
+++ TIL/260324_DAY15_TodoList_Project/todolist-app/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="todoItem in todoList"
+          :key="todoItem.id"
+          :todoItem="todoItem"
+          @delete-todo="$emit('delete-todo', $event)"
+          @toggle-completed="$emit('toggle-completed', $event)"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoListItem from './TodoListItem.vue';
+
+export default {
+  name: 'TodoList',
+  components: { 
+    TodoListItem 
+  },
+  props: {
+    // 부모로부터 받는 할 일 배열 (필수값)
+    todoList: { 
+      type: Array, 
+      required: true 
+    },
+  },
+  // 부모(App.vue)로 다시 보낼 이벤트 정의
+  emits: ['delete-todo', 'toggle-completed'],
+};
+</script>
